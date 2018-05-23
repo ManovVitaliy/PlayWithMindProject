79b49e83853720ad839b7dd56d10909c2623b471
Vitaliy Manov <manovvitaliy@gmail.com>
2018-05-23 14:35:50 +0300

    add firebase to PlaywithMindProject

--- PlayWithMindProject/PlayWithMind/PlayWithMind/ViewController.swift
+++ PlayWithMindProject/PlayWithMind/PlayWithMind/ViewController.swift
@@ -7,19 +7,14 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let firebase = Database.database().reference().child("table")
+        firebase.setValue(["key": "newValue"])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
