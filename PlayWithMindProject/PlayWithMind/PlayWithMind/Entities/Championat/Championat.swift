c4f98c67ec2a22cf47ff0f64baeb1786695e9741
Vitaliy Manov <manovvitaliy@gmail.com>
2018-09-03 12:28:29 +0300

    updated entities

--- PlayWithMindProject/PlayWithMind/PlayWithMind/Entities/Championat/Championat.swift
+++ PlayWithMindProject/PlayWithMind/PlayWithMind/Entities/Championat/Championat.swift
@@ -9,24 +9,32 @@
 import UIKit
 
 class Championat {
+    
+    // MARK: - keys
+    static let championatImageKey = "championatImage"
+    static let championatNameKey = "championatName"
+    
+    //MARK: - properties
     var championatImage: String = ""
     var championatName: String = ""
     
+    // convert model Championat to dictionary
     class func fromModelToDict(championat: Championat) -> [String: AnyObject] {
         var dict = [String: AnyObject]()
-        let dictHelper = ["name": championat.championatName,
-                          "image": championat.championatImage]
+        let dictHelper = [championatNameKey: championat.championatName,
+                          championatImageKey: championat.championatImage]
         dict = [championat.championatName: dictHelper] as [String : AnyObject]
         
         return dict
     }
     
+    // convert dictionary to Championat
     class func fromDictToModel(dictionary: [String: AnyObject]) -> Championat {
         let championat = Championat()
-        if let championatName = dictionary["name"] as? String {
+        if let championatName = dictionary[championatNameKey] as? String {
             championat.championatName = championatName
         }
-        if let championatImage = dictionary["image"] as? String {
+        if let championatImage = dictionary[championatImageKey] as? String {
             championat.championatImage = championatImage
         }
         
--- PlayWithMindProject/PlayWithMind/PlayWithMind/Entities/Country/Country.swift
+++ PlayWithMindProject/PlayWithMind/PlayWithMind/Entities/Country/Country.swift
@@ -9,24 +9,32 @@
 import UIKit
 
 class Country {
+    
+    //MARK: - keys
+    static let countryNameKey = "countryName"
+    static let countryImageKey = "countryImage"
+    
+    //MARK: - properties
     var countryImage: String = ""
     var countryName: String = ""
     
+    // convert model Player to dictionary
     class func fromModelToDict(country: Country) -> [String: AnyObject] {
         var dict = [String: AnyObject]()
-        let dictHelper = ["name": country.countryName,
-                          "image": country.countryImage]
+        let dictHelper = [Country.countryNameKey: country.countryName,
+                          Country.countryImageKey: country.countryImage]
         dict = [country.countryName: dictHelper] as [String : AnyObject]
         
         return dict
     }
     
+    // convert dictionary to Player
     class func fromDictToModel(dictionary: [String: AnyObject]) -> Country {
         let country = Country()
-        if let countryName = dictionary["name"] as? String {
+        if let countryName = dictionary[Country.countryNameKey] as? String {
             country.countryName = countryName
         }
-        if let countryImage = dictionary["image"] as? String {
+        if let countryImage = dictionary[Country.countryImageKey] as? String {
             country.countryImage = countryImage
         }
         
